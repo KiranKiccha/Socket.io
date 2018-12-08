e094099e2bc383b41f23be022f39500d3d5eb879
Sundaresh <kiran.sundaresh@accenture.com>
2018-12-08 13:01:08 +0530

    created socket.io fuction for listening events

--- Express/server/server.js
+++ Express/server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 const path = require('path');
 const publicPath = path.join(__dirname , '../public')
 
@@ -6,9 +8,22 @@ const Port = process.env.PORT || 3000
 
 const app = express();
 
+const server = http.createServer(app);
+
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log('New user Connected');
+    
+    socket.on('disconnect',()=>{
+        console.log('User got disconnected.....');
+        
+    })
+})
+
 app.use(express.static(publicPath));
 
-app.listen(Port,()=>{
+server.listen(Port,()=>{
     console.log(`Process is up and running on ${Port}`);
     
 })
